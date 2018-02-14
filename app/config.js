f853356a0556cfb9cf654b4f215e4db28746bd0c
Yuhang <abeyuhang@gmail.com>
2018-02-14 10:19:44 +0800

    update

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@ const pEnv = process.env;
 const logLevel = pEnv.LOG_LEVEL || (pEnv.NODE_ENV === 'production' ? 'info' : 'debug');
 const esHosts = pEnv.ES_HOSTS || '172.16.150.29:9200';
 const bulkInterval = Number(pEnv.BULK_INTERVAL || (pEnv.NODE_ENV === 'production' ? 3 * 60 * 1000 : 5 * 1000));
-
+const resetInterval = Number(pEnv.RESET_INTERVAL || 10 * 1000);
 const config = {
   server: {
     host: '0.0.0.0',
@@ -12,6 +12,7 @@ const config = {
     level: logLevel
   },
   api: {
+    resetInterval, 
     host: 'api.huobipro.com'
   },
   elastic: {
--- app/index.js
+++ app/index.js
@@ -2,11 +2,14 @@ const config = require('./config');
 const logger = require('./logger');
 const WebSocket = require('ws');
 const pako = require('pako');
-const moment = require('moment');
+const moment = require('moment-timezone');
 const elastcsearch = require('./elastic');
 const http = require('http');
-const TIME_FORMAT = 'YYYY/MM/DD HH:mm:ss';
+const TIME_FORMAT = 'YYYY/MM/DD HH:mm:ss.SSS';
+let autoId = 0;
+let receiveLastTickTimestamp = 0;
 const status = {
+  crawlerId: null,
   receiveTickCount: 0,
   receiveTickCountPerSecond: null,
   receiveLastTickTime: null,
@@ -14,7 +17,7 @@ const status = {
   bulkSuccessTickCount: 0,
   bulkErrorTickCount: 0,
   bulkLastTickTime: null,  
-  sysBootTime: moment().format(TIME_FORMAT),
+  sysBootTime: moment().tz('Asia/Shanghai').format(TIME_FORMAT),
   sysErrorCount: 0,
   sysLastErrorTime: null,
   sysLastErrorMessage: null
@@ -22,6 +25,7 @@ const status = {
 
 class BtcCrawler {
   constructor() {
+    this._id = (autoId++).toString(32);
     this._bootTime = Date.now() / 1000;
     this._tickQueue = [];
     this._bulkBusy = true;
@@ -33,13 +37,13 @@ class BtcCrawler {
   _checkES() {
     elastcsearch.ping(err => {
       if (err) {
-        logger.error('elasticsearch connect error, will try after 10 seconds');
+        logger.error('crawler', this._id, 'elasticsearch connect error, will try after 5 seconds');
         logger.error(err);
         setTimeout(() => {
           this._checkES();
-        }, 10000);
+        }, 5000);
       } else {
-        logger.info('elasticsearch connected');
+        logger.info('crawler', this._id, 'elasticsearch connected');
         this._bulkBusy = false;
         this._initialize();
       }
@@ -56,8 +60,9 @@ class BtcCrawler {
     this._bulk(ticks);
   }
   _pushTick(tick) {
+    // receiveLastTickTimestamp = tick.timestamp;
     this._tickQueue.push(tick);
-    const tm = moment(tick.timestamp).format('YYYY/MM/DD HH:mm:ss.SSS');
+    const tm = moment(tick.timestamp).tz('Asia/Shanghai').format(TIME_FORMAT);
     status.receiveLastTickTime = tm;
     status.recieveLastTickPrice = tick.price;
     status.receiveTickCount++;    
@@ -82,24 +87,18 @@ class BtcCrawler {
       this._bulkBusy = false;
       if (err) {
         status.bulkErrorTickCount += ticks.length;
-        this._onSysErr(err);
+        _onSysErr(err);
       } else {
         status.bulkSuccessTickCount += ticks.length;
-        status.bulkLastTickTime = moment(ticks[ticks.length - 1].timestamp).format('YYYY/MM/DD HH:mm:ss.SSS');
-        logger.info('write ticks', ticks.length);
+        status.bulkLastTickTime = moment(ticks[ticks.length - 1].timestamp).tz('Asia/Shanghai').format(TIME_FORMAT);
+        logger.info('crawler', this._id, 'write ticks', ticks.length);
       }
     });
-  }
-  _onSysErr(err) {
-    logger.error(err);
-    status.sysErrorCount++;
-    status.sysLastErrorTime = moment().format(TIME_FORMAT);
-    status.sysLastErrorMessage = err ? (err.message || err.toString()) : 'unkown';
   }
   _initialize() {
     const ws = new WebSocket(`wss://${config.api.host}/ws`);
     ws.on('open', () => {
-      logger.info('websocket connected');
+      logger.info('crawler', this._id, 'websocket connected');
       ws.send(JSON.stringify({
         sub: 'market.btcusdt.kline.1min',
         id: 'btcusdt'
@@ -122,38 +121,48 @@ class BtcCrawler {
           });
         }
       } catch(ex) {
-        this._onSysErr(ex);
+        _onSysErr(ex);
       }
     });
     ws.on('close', () => {
-      logger.info('websocket closed');
+      logger.info('crawler', this._id, 'websocket closed, will restart');
       this.destroy();
+      setImmediate(run);
     });
     ws.on('error', err => {
-      this._onSysErr(err);
+      logger.info('crawler', this._id, 'websocket error, will restart');      
+      _onSysErr(err);
       this.destroy();
+      setImmediate(run);
     });
     this._ws = ws;
   }
   destroy() {
     if (this._destried) return;
-    this._destried = true;    
+    this._destried = true;
     if (this._ws) {
-      this._ws.removeAllListeners();
-      this._ws.close();
-      this._ws = null;
+      try {
+        this._ws.removeAllListeners();
+        this._ws.close();
+        this._ws = null;
+      } catch(ex) {
+        _onSysErr(ex);
+      }
     }
     if (this._tickQueue.length > 0) {
       this._bulk(this._tickQueue);
     }
-    this._tickQueue = null;
+    this._tickQueue = null;    
     clearInterval(this._bulkTM);
-    logger.info('will restart after 3 seconds');
-    setTimeout(run, 3000);
   }
 }
 
-
+function _onSysErr(err) {
+  logger.error(err);
+  status.sysErrorCount++;
+  status.sysLastErrorTime = moment().tz('Asia/Shanghai').format(TIME_FORMAT);
+  status.sysLastErrorMessage = err ? (err.message || err.toString()) : 'unkown';
+}
 
 let crawler = null;
 function run() {
@@ -162,9 +171,26 @@ function run() {
 }
 /* bootstrap */
 run();
+setInterval(() => {
+  if (receiveLastTickTimestamp === null) return;
+  const now = Date.now();
+  logger.debug(config.api.resetInterval, now - receiveLastTickTimestamp);
+  if (now - receiveLastTickTimestamp >= config.api.resetInterval) {
+    logger.info('crawler', crawler._id, 'will restart because of long time no tick');
+    run();
+  }
+}, Math.floor(config.api.resetInterval / 2));
+
 
 http.createServer((req, res) => {
-  res.end(JSON.stringify(status, null, 2));
+  if (req.url === '/restart') {
+    logger.info('crawler', crawler._id, 'will restart as user command');
+    run();
+  }
+  status.crawlerId = crawler._id;
+  res.end(JSON.stringify(status, null, 2));  
+}).on('error', err => {
+  _onSysErr(err);
 }).listen(config.server.port, config.server.host, () => {
   logger.info(`crawler status server listening at http://${config.server.host}:${config.server.port}`);
 });
